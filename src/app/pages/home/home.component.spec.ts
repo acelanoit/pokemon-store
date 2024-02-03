2c461360df28eff2b0402d1639c1d9fea6f10389
antonio celano <antonio_celano@hotmail.com>
2024-02-03 19:31:42 +0100

    Add unit tests for home component and cart service

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -7,18 +7,161 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
+
+    // The TestBed.configureTestingModule method is used to configure and create an Angular testing module.
+    // TestBed.compileComponents() is an asynchronous operation that compiles all components in the testing module
+    // and returns a promise that is resolved when the compilation is done.
     await TestBed.configureTestingModule({
       imports: [...commonImports],
       declarations: [...commonDeclarations],
       providers: [...commonProviders]
     }).compileComponents();
 
+    // The TestBed.createComponent method is used to create an instance of a component.
+    // The fixture is like a box that holds your Angular component: It gives you access to the component instance and the component's DOM nodes.
     fixture = TestBed.createComponent(HomeComponent);
+
+    // The component instance is the object that represents the component itself. You can access the component instance from the fixture object.
     component = fixture.componentInstance;
+
+    // The fixture.detectChanges method triggers change detection in the component.
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the number of displayed items', () => {
+    component.onItemsCountChange(150);
+    expect(component.count).toBe(150);
+  });
+
+  it('should set the type of displayed items', () => {
+    component.onShowType({ name: 'fire', selected: true });
+    expect(component.type).toEqual({ name: 'fire', selected: true });
+  });
+
+  it('should set the number of columns', () => {
+    component.onColumnsCountChange(4);
+    expect(component.cols).toBe(4);
+  });
+
+  it('should sort the displayed items in ascending order', () => {
+    const items = [
+      {
+        id: 4,
+        name: 'Test 4',
+        price: 13,
+        'type(s)': 'Type 4',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 4'
+      },
+      {
+        id: 2,
+        name: 'Test 2',
+        price: 13,
+        'type(s)': 'Type 2',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 2'
+      },
+      {
+        id: 6,
+        name: 'Test 6',
+        price: 13,
+        'type(s)': 'Type 6',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 6'
+      }
+    ];
+    component.displayedPokemons = items;
+    component.onSortChange('asc');
+    expect(component.displayedPokemons).toEqual(
+      [
+        {
+          id: 2,
+          name: 'Test 2',
+          price: 13,
+          'type(s)': 'Type 2',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 2'
+        },
+        {
+          id: 4,
+          name: 'Test 4',
+          price: 13,
+          'type(s)': 'Type 4',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 4'
+        },
+        {
+          id: 6,
+          name: 'Test 6',
+          price: 13,
+          'type(s)': 'Type 6',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 6'
+        }
+      ]
+    );
+  });
+
+  it('should sort the displayed items in descending order', () => {
+    const items = [
+      {
+        id: 4,
+        name: 'Test 4',
+        price: 13,
+        'type(s)': 'Type 4',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 4'
+      },
+      {
+        id: 2,
+        name: 'Test 2',
+        price: 13,
+        'type(s)': 'Type 2',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 2'
+      },
+      {
+        id: 6,
+        name: 'Test 6',
+        price: 13,
+        'type(s)': 'Type 6',
+        image: 'https://example.com/image.jpg',
+        description: 'Description 6'
+      }
+    ];
+    component.displayedPokemons = items;
+    component.onSortChange('desc');
+    expect(component.displayedPokemons).toEqual(
+      [
+        {
+          id: 6,
+          name: 'Test 6',
+          price: 13,
+          'type(s)': 'Type 6',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 6'
+        },
+        {
+          id: 4,
+          name: 'Test 4',
+          price: 13,
+          'type(s)': 'Type 4',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 4'
+        },
+        {
+          id: 2,
+          name: 'Test 2',
+          price: 13,
+          'type(s)': 'Type 2',
+          image: 'https://example.com/image.jpg',
+          description: 'Description 2'
+        }
+      ]
+    );
   });
 });
--- src/app/services/cart.service.spec.ts
+++ src/app/services/cart.service.spec.ts
@@ -1,18 +1,119 @@
 import { TestBed } from '@angular/core/testing';
 import { CartService } from './cart.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CartItem } from '../models/cart.model';
+
+// NoopAnimationsModule is a module provided by Angular that disables animations for testing.
+// When we add an item to the cart, if that action triggers a change in the Material Badge
+// (like showing or updating the badge), it could be triggering an animation.
+// This module allows Angular to handle animation events without actually performing any animations.
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CartService', () => {
   let service: CartService;
 
   beforeEach(() => {
+
+    // The TestBed.configureTestingModule method is used to configure and create an Angular testing module.
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule, NoopAnimationsModule]
     });
+
+    // TestBed.inject is an Angular testing utility for dependency injection. It retrieves an instance of a service from the testing module.
+    // The reason for using TestBed.inject instead of just creating an instance of CartService directly (service = new CartService();)
+    // is that Angular's dependency injection system is used to manage the instances of services. By using TestBed.inject,
+    // you ensure that the service is provided with any dependencies it needs,
+    // and it follows the same mechanism as it would in a real Angular application.
     service = TestBed.inject(CartService);
+
+    // Clear the cart before each test
+    service.clearCart();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add an item to the cart', () => {
+    const item: CartItem = {
+      image: 'https://example.com/image.jpg',
+      name: 'Test Pokemon',
+      price: 13,
+      quantity: 1,
+      id: 24
+    };
+    service.addToCart(item);
+    service.cart.subscribe(cart => {
+      expect(cart.items).toContain(item);
+    });
+  });
+
+  it('should increment the quantity if the item is already in the cart', () => {
+    const item: CartItem = {
+      image: 'https://example.com/image.jpg',
+      name: 'Test Pokemon',
+      price: 13,
+      quantity: 1,
+      id: 31
+    };
+    service.addToCart(item);
+    service.addToCart(item);
+    service.cart.subscribe(cart => {
+      expect(cart.items.length).toBe(1);
+      expect(cart.items[0].quantity).toBe(2);
+    });
+  });
+
+  it('should reduce the quantity of an item', () => {
+    const item: CartItem = {
+      image: 'https://example.com/image.jpg',
+      name: 'Test Pokemon',
+      price: 13,
+      quantity: 3,
+      id: 53
+    };
+    service.addToCart(item);
+    service.removeQuantity(item);
+    service.cart.subscribe(cart => {
+      expect(cart.items[0].quantity).toBe(2);
+    });
+  });
+
+  it('should remove an item from the cart', () => {
+    const item: CartItem = {
+      image: 'https://example.com/image.jpg',
+      name: 'Test Pokemon',
+      price: 13,
+      quantity: 1,
+      id: 42
+    };
+    service.addToCart(item);
+    service.removeFromCart(item);
+    service.cart.subscribe(cart => {
+      expect(cart.items).not.toContain(item);
+    });
+  });
+
+  it('should calculate the total price of the items in the cart', () => {
+    const items: CartItem[] = [
+      {
+        image: 'https://example.com/image.jpg',
+        name: 'Test Pokemon',
+        price: 13,
+        quantity: 2,
+        id: 42
+      },
+      {
+        image: 'https://example.com/image.jpg',
+        name: 'Test Pokemon 2',
+        price: 10,
+        quantity: 1,
+        id: 43
+      }
+    ];
+    service.cart.next({ items });
+    service.cart.subscribe(cart => {
+      expect(service.getTotal(cart.items)).toBe(36);
+    });
   });
 });
